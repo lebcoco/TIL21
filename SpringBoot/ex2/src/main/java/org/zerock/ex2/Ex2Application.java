709c1f648d78d9f553d975cd5b42ad6f3a6b9a0d
lebcoco <leee24638@gmail.com>
2021-01-14 18:20:18 +0900

    Update 0114 springBoot ex2

--- SpringBoot/ex2/src/main/java/org/zerock/ex2/Ex2Application.java
+++ SpringBoot/ex2/src/main/java/org/zerock/ex2/Ex2Application.java
@@ -1,13 +1,25 @@
 package org.zerock.ex2;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.zerock.ex2.repository.DbDriver;
+import org.zerock.ex2.repository.MariaDbDriver;
 
 @SpringBootApplication
 public class Ex2Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(Ex2Application.class, args);
-  }
 
+    DbDriver dbDriver = new MariaDbDriver();
+
+
+    // ================================================
+    System.out.println(dbDriver.create());
+    System.out.println(dbDriver.read());
+    System.out.println(dbDriver.update());
+    System.out.println(dbDriver.delete());
+    // ================================================
+
+
+//    SpringApplication.run(Ex2Application.class, args);
+  }
 }
