f37392f2c14f2fdcb44293d2ddeb51dc5c823dba
lebcoco <leee24638@gmail.com>
2021-01-14 14:12:06 +0900

    Update 0114 springBoot ex2 조회작업test-findById

--- SpringBoot/ex2/src/test/java/org/zerock/ex2/repository/MemoRepositoryTest.java
+++ SpringBoot/ex2/src/test/java/org/zerock/ex2/repository/MemoRepositoryTest.java
@@ -6,7 +6,6 @@ import java.util.stream.IntStream;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 import org.zerock.ex2.entity.Memo;
 
 @SpringBootTest
@@ -26,6 +25,18 @@ class MemoRepositoryTests {
       Memo memo = Memo.builder().memoText("Sample..."+i).build();
       memoRepository.save(memo);
     });
+  }
+
+  @Test
+  public void testSelect(){
+//데이터베이스에 존재하는 mno
+    Long mno  = 100L;
+    Optional<Memo> result = memoRepository.findById(mno);
+    System.out.println("==================================");
+    if(result.isPresent()){
+      Memo memo = result.get();
+      System.out.println(memo);
+    }
   }
 
 }
