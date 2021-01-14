a07226cbbdcc93b12343bcb9d5f4205f555994e1
lebcoco <leee24638@gmail.com>
2021-01-14 14:23:03 +0900

    Update 0114 springBoot ex2 테스트 주석

--- SpringBoot/ex2/src/test/java/org/zerock/ex2/repository/MemoRepositoryTest.java
+++ SpringBoot/ex2/src/test/java/org/zerock/ex2/repository/MemoRepositoryTest.java
@@ -16,37 +16,43 @@ class MemoRepositoryTests {
   MemoRepository memoRepository;
 
   @Test
-  public void testClass(){
+  public void testClass() {
     System.out.println(memoRepository.getClass().getName());
   }
 
+  // 등록 작업 테스트
   @Test
-  public void testInsertDummies(){
-    IntStream.rangeClosed(1,100).forEach(i -> {
-      Memo memo = Memo.builder().memoText("Sample..."+i).build();
+  public void testInsertDummies() {
+    IntStream.rangeClosed(1, 100).forEach(i -> {
+      Memo memo = Memo.builder().memoText("Sample..." + i).build();
       memoRepository.save(memo);
     });
   }
 
+  // 조회 작업 테스트 - findById()
   @Test
-  public void testSelect(){
-//데이터베이스에 존재하는 mno
-    Long mno  = 100L;
+  public void testSelect() {
+    //데이터베이스에 존재하는 mno
+    Long mno = 100L;
     Optional<Memo> result = memoRepository.findById(mno);
     System.out.println("==================================");
-    if(result.isPresent()){
+    if (result.isPresent()) {
       Memo memo = result.get();
       System.out.println(memo);
     }
   }
+
+  // 조회 작업 테스트 - getOne()
   @Transactional
   @Test
-  public void testSelect2(){
-//데이터베이스에 존재하는 mno
-    Long mno  = 100L;
+  public void testSelect2() {
+    //데이터베이스에 존재하는 mno
+    Long mno = 100L;
     Memo memo = memoRepository.getOne(mno);
-    System.out.println("===============================");   System.out.println(memo);
+    System.out.println("===============================");
+    System.out.println(memo);
   }
+
   // 수정 작업 테스트
   @Test
   public void testUpdate() {
