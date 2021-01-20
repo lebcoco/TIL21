f72bb5393e1f0ee60b4ad2789ee0791c463e0c20
lebcoco <leee24638@gmail.com>
2021-01-20 18:45:54 +0900

    Upload 0120 springBoot Part02-서비스계층의 테스트

--- SpringBoot/miniProject/src/main/java/org/zerock/guestbook/service/GuestbookService.java
+++ SpringBoot/miniProject/src/main/java/org/zerock/guestbook/service/GuestbookService.java
@@ -1,7 +1,17 @@
 package org.zerock.guestbook.service;
-
 import org.zerock.guestbook.dto.GuestbookDTO;
+import org.zerock.guestbook.entity.Guestbook;
 // 방명록 등록하는 시나리오 처리
 public interface GuestbookService {
   Long register(GuestbookDTO dto);
+  // default기능을 활용해 구현 클래스에서 동작할 수 있는 dtoToEntity()구성
+  default Guestbook dtoToEntity(GuestbookDTO dto) {
+    Guestbook entity = Guestbook.builder()
+        .gno(dto.getGno())
+        .title(dto.getTitle())
+        .content(dto.getContent())
+        .writer(dto.getWriter())
+        .build();
+    return entity;
+  }
 }
