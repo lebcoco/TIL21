3223e8248aa62199148f3761c070aefa1a9f1e79
lebcoco <leee24638@gmail.com>
2021-01-22 18:18:50 +0900

    Update 0121 springBoot part2_목록데이터페이지처리

--- SpringBoot/miniProject/src/main/java/org/zerock/guestbook/dto/PageResultDTO.java
+++ SpringBoot/miniProject/src/main/java/org/zerock/guestbook/dto/PageResultDTO.java
@@ -3,15 +3,61 @@ package org.zerock.guestbook.dto;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import lombok.Data;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 //JPA를 이용하는 Repository에서는 페이지 처리 결과를 Page<Entity> 타입으로 반환하게 된다.
 //Page<Entity>의 객체물을 DTO객체로 변환해서 자료구조로 담아줘야 한다.
-@Data
-public class PageResultDTO<DTO, EN> {
-  private List<DTO> dtoList;
-  public PageResultDTO(Page<EN> result , Function<EN, DTO> fn){ //Function<EN,DTO>는 엔티티 객체들을 DTO로 변환해 주는 기능.
-    dtoList = result.stream().map(fn).collect(Collectors.toList());
+  @Data
+  public class PageResultDTO<DTO, EN> {
+
+    //DTO리스트
+    private List<DTO> dtoList;
+
+    //총 페이지 번호
+    private int totalPage;
+
+    //현재 페이지 번호
+    private int page;
+    //목록 사이즈
+    private int size;
+
+    //시작 페이지 번호, 끝 페이지 번호
+    private int start, end;
+
+    //이전, 다음
+    private boolean prev, next;
+
+    //페이지 번호  목록
+    private List<Integer> pageList;
+
+    public PageResultDTO(Page<EN> result, Function<EN, DTO> fn) {
+
+      dtoList = result.stream().map(fn).collect(Collectors.toList());
+
+      totalPage = result.getTotalPages();
+
+      makePageList(result.getPageable());
+    }
+
+    private void makePageList(Pageable pageable) {
+
+      this.page = pageable.getPageNumber() + 1; // 0부터 시작하므로 1을 추가
+      this.size = pageable.getPageSize();
+
+      //temp end page
+      int tempEnd = (int) (Math.ceil(page / 10.0)) * 10;
+
+      start = tempEnd - 9;
+
+      prev = start > 1;
+
+      end = totalPage > tempEnd ? tempEnd : totalPage;
+
+      next = totalPage > tempEnd;
+
+      pageList = IntStream.rangeClosed(start, end).boxed().collect(Collectors.toList());
+    }
   }
-}
