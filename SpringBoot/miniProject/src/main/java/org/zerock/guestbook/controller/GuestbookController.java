6ced608698e5c86197152bf77e4962f775e345b5
lebcoco <leee24638@gmail.com>
2021-01-28 19:55:17 +0900

    Upload 0128 springBoot Part02-4-7 register

--- SpringBoot/miniProject/src/main/java/org/zerock/guestbook/controller/GuestbookController.java
+++ SpringBoot/miniProject/src/main/java/org/zerock/guestbook/controller/GuestbookController.java
@@ -5,7 +5,10 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.guestbook.dto.GuestbookDTO;
 import org.zerock.guestbook.dto.PageRequestDTO;
 import org.zerock.guestbook.service.GuestbookService;
 
@@ -29,5 +32,27 @@ public class GuestbookController {
     log.info("list............." + pageRequestDTO);
 
     model.addAttribute("result", service.getList(pageRequestDTO));
+  }
+
+  // 등록 페이지와 등록 처리
+  // GET방식: 화면을 보여주고, POST방식: 처리 후 목록 페이지로 이동
+  // RedirectAttributes를 이용해 한 번만 화면에서 'msg'라는 이름의 변수를 사용할 수 있도록 처리
+  // addFlashAttribute() 단 한 번만 데이터를 전달하는 용도
+  @GetMapping("/register")
+  public void register(){
+    log.info("regiser get...");
+  }
+
+  @PostMapping("/register")
+  public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes){
+
+    log.info("dto..." + dto);
+
+    //새로 추가된 엔티티의 번호
+    Long gno = service.register(dto);
+
+    redirectAttributes.addFlashAttribute("msg", gno);
+
+    return "redirect:/guestbook/list";
   }
 }
